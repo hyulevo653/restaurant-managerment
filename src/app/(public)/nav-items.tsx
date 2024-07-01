013faffe3d3a2723f415f83eea96d0a3ee6e7f55
hyulevo653 <duchuy653201@gmail.com>
2024-07-01 16:43:35 +0700

    .

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,26 +1,36 @@
 'use client'
 import { useAppContext } from '@/components/app-provider'
+import { Role } from '@/constants/type'
+import { cn, handleErrorApi } from '@/lib/utils'
+import { useLogoutMutation } from '@/queries/useAuth'
+import { RoleType } from '@/types/jwt.types'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
-const menuItems = [
+const menuItems : {
+  title: string 
+  href:string 
+  role?: RoleType[]
+  hideWhenLogin?: boolean
+}[] = [
   {
-    title: 'Món ăn',
-    href: '/menu' // authRequired: undefined nghĩa là đăng nhập hay chưa đều hiển thị
+    title: 'Trang chủ',
+    href: '/'
   },
   {
-    title: 'Đơn hàng',
-    href: '/orders', 
-    authRequired: true  
+    title: 'Menu',
+    href : '/guest/menu',
+    role: [Role.Guest]
   },
   {
     title: 'Đăng nhập',
     href: '/login',
-    authRequired: false // khi false nghia là chưa đăng nhập thì sẽ hiển thị
+    hideWhenLogin: true
   },
   {
     title: 'Quản lý',
     href: '/manage/dashboard',
-    authRequired: true // khi true nghĩa là đăng nhập rồi mới hiển thị
+    role : [Role.Owner,Role.Employee]
   }
 ]
 // Server: Món ăn, Đăng nhập. Do server không biết trạng thái đăng nhập của user
@@ -29,14 +39,45 @@ const menuItems = [
 
 
 export default function NavItems({ className }: { className?: string }) {
-  const { isAuth } = useAppContext()
+  const { role,setRole } = useAppContext()
+  const logoutMutation = useLogoutMutation()
+  const router = useRouter()
+  const logout = async () => {
+    if (logoutMutation.isPending) return
+    try {
+      await logoutMutation.mutateAsync()
+      setRole()
+      router.push('/')
+    } catch (error: any) {
+      handleErrorApi({
+        error
+      })
+    }
+  }
   
-  return menuItems.map((item) => {
-    if( (item.authRequired == false && isAuth) || (item.authRequired == true && !isAuth)) return null
-    return (
-      <Link href={item.href} key={item.href} className={className}>
-        {item.title}
-      </Link>
-    )
-  })
+  return (
+    <>
+      {menuItems.map((item) => {
+        // Trường hợp đăng nhập thì chỉ hiển thị menu đăng nhập
+        const isAuth = item.role && role && item.role.includes(role)
+        // Trường hợp menu item có thể hiển thị dù cho đã đăng nhập hay chưa
+        const canShow =
+          (item.role === undefined && !item.hideWhenLogin) ||
+          (!role && item.hideWhenLogin)
+        if (isAuth || canShow) {
+          return (
+            <Link href={item.href} key={item.href} className={className}>
+              {item.title}
+            </Link>
+          )
+        }
+        return null
+      })}
+      {role && (
+        <div className={cn(className, 'cursor-pointer')} onClick={logout}>
+          Đăng xuất
+        </div>
+      )}
+    </>
+  )
 }
